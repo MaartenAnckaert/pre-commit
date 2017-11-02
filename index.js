84e27d8fdc5fd81e5bcd9233fb0713481fe86ee0
Maarten Anckaert <maarten.anckaert@icapps.com>
2017-11-02 13:42:59 +0100

    Customize for Sam, to add full customisation in the future!

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ Hook.log = {
   run     : [ "We have nothing pre-commit hooks to run. Either you're missing the `scripts`", 'in your `package.json` or have configured pre-commit to run nothing.', 'Skipping the pre-commit hook.' ].join('\n'),
 
   failure : [
-    this.config.message && this.config.message,
+    'Sammeke alstublieft he!!! Maakt dat in orde is en geen debuggers in u code he!',
     "We've failed to pass the specified git pre-commit hooks as the `%s`",
     "hook returned an exit code (%d). If you're feeling adventurous you can",
     'skip the git pre-commit hooks by adding the following flags to your commit:',
